458e2d8073043cdb78e46bdd46942d334c1f534c
Le-Vu Tran <tlvu2697@outlook.com>
2018-08-17 10:52:21 +0700

    Update

---
+++ clean.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+rm *.aux
+rm *.log
+rm *.out
+rm *.toc
+rm *.fdb_latexmk
+rm *.fls
--- commit.sh
+++ commit.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+sh clean.sh
+
 git add .
 git commit -m "Update"
 git push origin master
