d903aac4dd171ade49d313f9cee3a0ca823b4114
Le-Vu Tran <tlvu2697@outlook.com>
2018-08-24 15:44:23 +0700

    Init Week 12

--- commit.sh
+++ commit.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-sh clean.sh
+commit_message="$1"
+
+if [ "$commit_message" = "" ]; then
+	echo "Please provide Commit message"
+	exit
+fi
 
 git add .
-git commit -m "Update"
+git commit -m "$commit_message"
 git push origin master
